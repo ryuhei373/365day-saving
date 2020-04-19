7f1fe49d06559e875377358fb0cb1fee63a6c493
ryuhei4k9 <ryuhei.4k9@gmail.com>
2020-04-19 19:35:51 +0900

    実機で確認できるように

--- components/Cell.vue
+++ components/Cell.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-on:click="doneToggle" v-bind:class="{ 'bg-yellow-300': isDone }" class="relative flex justify-center items-center aspect-ratio-for-cell border-gray-600 border-solid border-b-2 border-r-2">
-    <span class="absolute top-for-cell w-full h-0 text-2xl text-gray-500">{{number}}</span>
+    <span class="absolute top-for-cell w-full h-0 text-2xl text-gray-500">{{ number }}</span>
   </div>
 </template>
 
